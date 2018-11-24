96012a17ec69f573f187ee66b09157ebf43ed03b
aksatyam <ashis@cybertrontechnologies.com>
2018-11-24 12:19:13 +0530

    changes done

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Router, Scene } from 'react-native-router-flux'
+import { Router, Scene, Lightbox } from 'react-native-router-flux'
 import Home from './pages/Home'
 import Friends from './pages/Friends'
 
 const AppNavigator = () => {
     return(
         <Router>
-        <Scene key = "root">
-            <Scene key = "home" component = {Home} title = "Home" initial = {true} />
-            <Scene key = "friends" component = {Friends} title = "Friends" />
-        </Scene>
-    </Router>
+            <Lightbox>
+                <Scene key = "root">
+                    <Scene key = "home" component = {Home} title = "Home" initial = {true} />
+                    <Scene key = "friends" component = {Friends} title = "Friends" />
+                </Scene>
+            </Lightbox>
+        </Router>
     )
 }
 
--- pages/Friends.js
+++ pages/Friends.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import {StyleSheet, Text, View, FlatList, Platform} from 'react-native';
 
 export default class Friends extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-       <TouchableOpacity>
-            <Text>Welcome to Friends Page</Text>
-        </TouchableOpacity>
-      </View>
+        <FlatList
+          data={[
+            {firstName: 'Devin', lastName: 'Test'},
+            {firstName: 'Jackson', lastName: 'Test'},
+            {firstName: 'James', lastName: 'Test'},
+            {firstName: 'Joel', lastName: 'Test'},
+            {firstName: 'John', lastName: 'Test'},
+            {firstName: 'Jillian', lastName: 'Test'},
+            {firstName: 'Jimmy', lastName: 'Test'},
+            {firstName: 'Julie', lastName: 'Test'},
+            {firstName: 'Devin', lastName: 'Test'},
+            {firstName: 'Jackson', lastName: 'Test'},
+            {firstName: 'James', lastName: 'Test'},
+            {firstName: 'Joel', lastName: 'Test'},
+            {firstName: 'John', lastName: 'Test'},
+            {firstName: 'Jillian', lastName: 'Test'},
+            {firstName: 'Jimmy', lastName: 'Test'},
+            {firstName: 'Julie', lastName: 'Test'},
+            {firstName: 'Devin', lastName: 'Test'},
+            {firstName: 'Jackson', lastName: 'Test'},
+            {firstName: 'James', lastName: 'Test'},
+            {firstName: 'Joel', lastName: 'Test'},
+            {firstName: 'John', lastName: 'Test'},
+            {firstName: 'Jillian', lastName: 'Test'},
+            {firstName: 'Jimmy', lastName: 'Test'},
+            {firstName: 'Julie', lastName: 'Test'},
+            {firstName: 'Devin', lastName: 'Test'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.firstName} {' '}{item.lastName}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
     );
   }
 }
@@ -17,8 +42,11 @@ export default class Friends extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
+    paddingTop: 22
+   },
+   item: {
+     padding: 10,
+     fontSize: 18,
+     height: 44,
+   },
 });
